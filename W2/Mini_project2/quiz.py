b620f54c46b7c0dae8c7c93c66081e5b91e5b268
Midoirl <midokin7@gmail.com>
2025-05-25 11:22:41 +0300

    .

---
+++ W2/Mini_project2/quiz.py
@@ -0,0 +1,18 @@
+#QUIZ
+
+#1 class is a user-defined blueprint or prototype from which objects are created. It defines a set of attributes and methods.
+
+# 2. An instance is a concrete object created based on the structure defined by a class.
+
+# 3. Encapsulation is the concept of bundling data and methods that operate on that data within one unit, typically a class.
+
+# 4. Abstraction means hiding complex implementation details and showing only the necessary features of an object.
+
+
+# 5. Inheritance is a mechanism where a new class derives properties and behaviors (methods) from an existing class.
+
+# 6.Multiple inheritance allows a class to inherit from more than one base class.
+
+# 7. Polymorphism allows methods to do different things based on the object it is acting upon, even if they share the same name.
+
+# 8.MRO defines the order in which Python looks for a method in a hierarchy of classes. It uses the C3 linearization algorithm.
